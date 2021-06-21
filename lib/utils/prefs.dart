97cc09e8e4af41a8f4c749a444e469a89cdbf3e4
黃智輝 <cpmoviephil@gmail.com>
2021-06-22 05:32:51 +0800

    +shared prefs

---
+++ lib/utils/prefs.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class MainPrefs {
+
+  final SharedPreferences prefs;
+
+  MainPrefs({@required this.prefs});
+
+  int getInt({@required String key,}) {
+   return prefs.getInt(key);
+  }
+
+  String getString({@required String key,}) {
+    return prefs.getString(key);
+  }
+
+  bool getBool({@required String key,}) {
+    return prefs.getBool(key);
+  }
+
+  Future<bool> setInt({@required String key, @required int value}) async {
+    return await prefs.setInt(key, value);
+  }
+
+  Future<bool> setString({@required String key, @required String value}) async {
+    return await prefs.setString(key, value);
+  }
+
+  Future<bool> setBool({@required String key, @required bool value}) async {
+    return await prefs.setBool(key, value);
+  }
+}
